b5aab4281a4ffee8beadb38bbd7a2f6717608fbc
S3877629 <73870711+mookee2809@users.noreply.github.com>
2021-08-28 19:40:07 +0700

    upload main and fix few things

---
+++ util/MyToys.java
@@ -0,0 +1,113 @@
+package util;
+
+import java.util.*;
+
+public class MyToys {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static int getAnInteger(String inputMsg, String errorMsg) {
+        int n;
+        while (true) {
+            try {
+                System.out.print(inputMsg);
+                n = Integer.parseInt(sc.nextLine());
+                return n;
+            } catch (Exception e) {
+                System.out.println(errorMsg);
+            }
+        }
+    }
+
+    public static int getAnInteger(String inputMsg, String errorMsg, int lowerBound, int upperBound) {
+        int n, tmp;
+
+        if (lowerBound > upperBound) {
+            tmp = lowerBound;
+            lowerBound = upperBound;
+            upperBound = tmp;
+        }
+        while (true) {
+            try {
+                System.out.print(inputMsg);
+                n = Integer.parseInt(sc.nextLine());
+                if (n < lowerBound || n > upperBound) {
+                    throw new Exception();
+                }
+                return n;
+            } catch (Exception e) {
+                System.out.println(errorMsg);
+            }
+        }
+    }
+
+    public static double getADouble(String inputMsg, String errorMsg) {
+        double n;
+        while (true) {
+            try {
+                System.out.print(inputMsg);
+                n = Double.parseDouble(sc.nextLine());
+                return n;
+            } catch (Exception e) {
+                System.out.println(errorMsg);
+            }
+        }
+    }
+
+    public static double getADouble(String inputMsg, String errorMsg, double lowerBound, double upperBound) {
+        double n, tmp;
+
+        if (lowerBound > upperBound) {
+            tmp = lowerBound;
+            lowerBound = upperBound;
+            upperBound = tmp;
+        }
+        while (true) {
+            try {
+                System.out.print(inputMsg);
+                n = Double.parseDouble(sc.nextLine());
+                if (n < lowerBound || n > upperBound) {
+                    throw new Exception();
+                }
+                return n;
+            } catch (Exception e) {
+                System.out.println(errorMsg);
+            }
+        }
+    }
+
+    public static String getDate(String inputMsg, String errorMsg, String format) {
+        String id;
+        boolean match;
+        while (true) {
+            System.out.print(inputMsg);
+            id = sc.nextLine().trim().toUpperCase();
+            match = id.matches(format);
+            if (id.length() == 0 || id.isEmpty() || match == false) {
+                System.out.println(errorMsg);
+            } else {
+                return id;
+            }
+        }
+    }
+
+    public static String getString(String inputMsg, String errorMsg) {
+        String id;
+        while (true) {
+            System.out.print(inputMsg);
+            id = sc.nextLine().trim();
+            if (id.length() == 0 || id.isEmpty()) {
+                System.out.println(errorMsg);
+            } else {
+                return id;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String date = getDate("Input Date(Date/Month/Year)): ", "Your input must be under "
+                + "the format of (Date/Month/Year), Date/Month/Year stands for digits",
+                "^(([0]?[1-9]|1[012])/([0]?[1-9]|[12][0-9]|3[01])/(2020|2021))");
+        System.out.println("Your Date: " + date);
+    }
+
+}
