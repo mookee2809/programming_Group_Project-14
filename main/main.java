d280920c65b32d4352a5dc1d5bc16eb299e24c6b
S3877629 <73870711+mookee2809@users.noreply.github.com>
2021-08-31 11:34:00 +0700

    Update main.java

--- main/main.java
+++ main/main.java
@@ -7,7 +7,7 @@ import data.DataGrouping;
 public class main {
 
     public static void main(String[] args) {
-        String filepath = "C:\\Users\\AN515-55.LAPTOP-VMBQ4GNS\\Desktop\\FPT_SE\\PRO192\\Suk\\Csv\\src\\csv\\covid-data.csv";
+        String filepath = "covid-data.csv";
         String searchTearm = "3/9/2020";
         DataGrouping data = new DataGrouping();
         data.dateSearch(searchTearm, filepath);
