8b664a92c1fffe9295112f5d9c4b442f794708ac
S3877629 <73870711+mookee2809@users.noreply.github.com>
2021-08-31 21:45:18 +0700

    Update main.java

--- main/main.java
+++ main/main.java
@@ -2,15 +2,36 @@
 package main;
 
 import data.DataGrouping;
+import data.Summary;
 
+import util.Menu;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
-        String filepath = "covid-data.csv";
-        String searchTearm = "3/9/2020";
-        DataGrouping data = new DataGrouping();
-        data.dateSearch(searchTearm, filepath);
+
+        int choice;
+        Menu menu = new Menu("Covid Data analsys");
+        menu.addNewOption("1. A pair of start date and end date");
+        menu.addNewOption("2. A number of days or weeks from a specific date");
+        menu.addNewOption("3. A number of days or weeks to a specific date");
         
+        
+        Summary summary = new Summary();
+        
+        do {
+            menu.printMenu();
+            choice = menu.getChoice();
+            switch (choice) {
+                case 1:
+                    summary.StartEndDate();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        } while (choice != 3);
     }
 }
+
