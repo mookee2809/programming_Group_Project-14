e09365d57cc37a0e14b477ada4c78feed1a14fc2
S3877629 <73870711+mookee2809@users.noreply.github.com>
2021-09-01 17:53:06 +0700

    Update Main.java

--- main/main.java
+++ main/main.java
@@ -10,28 +10,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int choice;
-        Menu menu = new Menu("Covid Data analsys");
-        menu.addNewOption("1. A pair of start date and end date");
+        
+        Menu menu = new Menu("Covid Data Analysis");
+        menu.addNewOption("Please choose 1 of the following option!\n1. A pair of start date and end date");
         menu.addNewOption("2. A number of days or weeks from a specific date");
         menu.addNewOption("3. A number of days or weeks to a specific date");
-        
-        
-        Summary summary = new Summary();
-        
+
+        Summary sum = new Summary();
+
+        int choice;
         do {
+
             menu.printMenu();
             choice = menu.getChoice();
             switch (choice) {
                 case 1:
-                    summary.StartEndDate();
+                    sum.StartEndDate();
                     break;
                 case 2:
+                    
                     break;
                 case 3:
+                    System.out.println("Good bye");
                     break;
             }
         } while (choice != 3);
     }
 }
+
 
